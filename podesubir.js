279f9971dcfc54a0fca16206fb5856d485de40b3
YasminTrentini <yasminft15@gmail.com>
2021-06-15 08:11:47 -0300

    atividade de controle de fluxo

---
+++ podesubir.js
@@ -0,0 +1,11 @@
+function podeSubir(altura,acompanhada){
+    if (podeSubir1,40,true)  {
+        console.log("pode subir");
+    
+     if (podeSubir <=1,40,true)
+        console.log("pode subir acompanhada");
+    
+    else console.log("não pode subir");
+    }
+}
+podeSubir(1,40,true)
